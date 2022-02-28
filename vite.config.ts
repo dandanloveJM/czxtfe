6a22c6df6f39d80943e9eed397f39fe2e8d509b5
dandanloveJM <gigi834921748@qq.com>
2022-02-28 19:27:30 +0800

    修改版本，解决加了Legacy plugin之后本地dev无法运行的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-const path = require('path')
-import { loadEnv } from 'vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+const path = require("path");
+import { loadEnv } from "vite";
+
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [vue(), vueJsx()],
+  base: "/",
+  plugins: [
+    vue(),
+    vueJsx(),
+    {
+      ...legacy({
+        targets: ["defaults", "not IE 11"],
+      }),
+      apply: "build",
+    },
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
- 
+
   // server: {
   //   port: 3000,
   //   proxy: {
@@ -24,4 +35,4 @@ export default defineConfig({
   //     }
   //   }
   // }
-})
+});
