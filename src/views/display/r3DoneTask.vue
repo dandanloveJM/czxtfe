55f0e79a2c9c24464d6e521973c33e6a0e79f120
dandanloveJM <gigi834921748@qq.com>
2022-02-28 22:04:11 +0800

    R3已完成任务添加合计

--- src/views/display/r3DoneTask.vue
+++ src/views/display/r3DoneTask.vue
@@ -37,31 +37,43 @@
         <a-table
           :columns="columns"
           :data-source="state.taskList"
-          :rowKey="(record) => record.processId"
+          :rowKey="(record) => record.name"
           :loading="tableLoading"
+          :pagination="false"
         >
           <template #type="{ record }">
-            <span>{{ TYPE_MAP[record.type] }}</span>
+            <span>{{ TYPE_MAP[record.type] || "" }}</span>
           </template>
           <template #updatedAt="{ record }">
-            <span>{{ changeTime(record.updatedAt) }}</span>
+            <span v-if="record.name != '合计'">{{
+              changeTime(record.updatedAt)
+            }}</span>
           </template>
 
           <template #totalPercentage="{ record }">
-            <a-tag v-if="record.totalPercentage < 100" color="warning">{{
-              record.totalPercentage + "%"
-            }}</a-tag>
-            <span v-else>{{ record.totalPercentage + "%" }}</span>
+            <a-tag
+              v-if="record.name != '合计' && record.totalPercentage < 100"
+              color="warning"
+              >{{ record.totalPercentage + "%" || "" }}</a-tag
+            >
+            <span
+              v-else-if="record.name != '合计' && record.totalPercentage == 100"
+              >{{ record.totalPercentage + "%" }}</span
+            >
           </template>
           <template #attachment="{ record }">
-            <a-button @click="() => showImg(record.attachment)"
+            <a-button
+              v-if="record.name != '合计'"
+              @click="() => showImg(record.attachment)"
               >查看任务书</a-button
             >
           </template>
           <template #products="{ record }">
-            <a-button @click="() => showProducts(record.products)"
-              >查看产值
-            </a-button>
+            <a-button
+              v-if="record.name != '合计'"
+              @click="() => showProducts(record.products)"
+              >查看产值</a-button
+            >
           </template>
         </a-table>
       </div>
